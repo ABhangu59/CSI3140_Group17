9384518a1adc5f751e3f940d8b709955703fb128
Ali B <alibhangu1@outlook.com>
2024-06-04 17:19:48 -0400

    Updated Tic-Tac-Toe Functionality
    Added in the computer player and some helper functions.
    
    Co-Authored-By: Tolu Emoruwa <105876577+EmoruwaTolu@users.noreply.github.com>

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -2,6 +2,8 @@ const board = document.getElementById('board');
 const squares = document.querySelectorAll('.square');
 var userTurn = true;
 let gameBoard = ["", "", "", "", "", "", "", "", ""];
+var numberOfPlayerWins = 0;
+var numberOfComputerWins = 0;
 
 
 const win_combos = [
@@ -28,14 +30,54 @@ squares.forEach(square => {
         userTurn = false;
 
         computerTurn();
+        if (checkWinner(gameBoard, "X")) {
+            alert("Player wins!");
+            numberOfPlayerWins++;
+            console.log(numberOfPlayerWins)
+            resetGame();
+        } else if (gameBoard.every(cell => cell !== "")) {
+            alert("It's a draw!");
+            resetGame();
+        } 
         
         console.log(gameBoard);
     });
 });
 
-function computerTurn(){
+function computerTurn() {
+    let emptySquares = gameBoard.map((val, index) => val === "" ? index : null).filter(val => val !== null);
+    if (emptySquares.length > 0) {
+        let randomIndex = emptySquares[Math.floor(Math.random() * emptySquares.length)];
+        gameBoard[randomIndex] = "O";
+        squares[randomIndex].textContent = "O";
+        if (checkWinner(gameBoard, "O")) {
+            alert("Computer wins!");
+            numberOfComputerWins++;
+            console.log(numberOfComputerWins)
+            resetGame();
+        } else if (gameBoard.every(cell => cell !== "")) {
+            alert("It's a draw!");
+            resetGame();
+        } else {
+            userTurn = true;
+        }
+    }
+}
 
+function checkWinner(board, player) {
+    return win_combos.some(combo => {
+        return combo.every(index => {
+            return board[index] === player;
+        });
+    });
+}
 
+function resetGame() {
+    gameBoard = ["", "", "", "", "", "", "", "", ""];
+    squares.forEach(square => {
 
+        const child = square.querySelector('.X');;
+        console.log(child)
+    });
     userTurn = true;
 }
